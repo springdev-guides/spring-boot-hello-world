cf0bfff3c1728835b8424b71c05edf8ffa5d0ea2
rainboyan <rainboyan@gmail.com>
2019-01-15 00:31:25 +0800

    Hello World

---
+++ complete/src/main/java/org/springdev/guides/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package org.springdev.guides.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+}
